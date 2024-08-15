2aeea61a1fe643a4f71d4120abb09c12d97e5e29
Francisco Cano Marchal <fcanomar@gmail.com>
2024-08-15 18:55:57 +0200

    salary rules in structure; minor tweaks seqs, category..

--- __manifest__.py
+++ __manifest__.py
@@ -1,18 +1,20 @@
 {
-    'name': 'Nóminas - Agua de Coco',
+    'name': 'Nominas - Agua de Coco',
     'version': '14.0.1.0.0',
     'category': 'Custom',
     'depends': [
         'payroll',
         'hr',
+        'hr_contract',
+        'resource',
     ],
     'data': [
         'views/hr_employee_views.xml',
         'views/resource_calendar_views.xml',
         'views/hr_contract_views.xml',
         'data/hr.salary.rule.category.csv',
-        'data/hr.payroll.structure.csv',
         'data/hr.salary.rule.csv',
         'data/hr.rule.input.csv',
+        'data/hr.payroll.structure.csv',
     ],
 }
